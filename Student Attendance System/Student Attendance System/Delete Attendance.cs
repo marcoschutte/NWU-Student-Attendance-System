90867968650ff2f326b61a20004540dfec570976
marcoschutte <65155839+marcoschutte@users.noreply.github.com>
2020-10-17 09:32:06 +0200

    Commit Delete Attendance

---
+++ Student Attendance System/Student Attendance System/Delete Attendance.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace Test
+{
+    public partial class Delete_Attendance : Form
+    {
+        public Delete_Attendance()
+        {
+            InitializeComponent();
+        }
+
+        SqlCommand command;
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataSet dataset;
+
+        public string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\marco\Desktop\Test\Test\Database1.mdf; Integrated Security = True";
+        public string attendanceID;
+
+        public void DisplayTable()
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            string selectAllQuery = "SELECT * FROM ATTENDANCE";
+
+            command = new SqlCommand(selectAllQuery, connection);
+            dataset = new DataSet();
+            adapter = new SqlDataAdapter();
+
+            adapter.SelectCommand = command;
+            adapter.Fill(dataset, "ATTENDANCE");
+            dataGridView1.DataSource = dataset;
+            dataGridView1.DataMember = "ATTENDANCE";
+
+            connection.Close();
+        }
+
+        public void ClearFields()
+        {
+            txtAttendanceID.Text = "";
+        }
+
+        public int CheckIfRecordExists()
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            string existQuery = "SELECT count([Attendance_ID]) FROM ATTENDANCE WHERE ([Attendance_ID] = '" + attendanceID + "')";
+
+            SqlCommand checkRecord = new SqlCommand(existQuery, connection);
+
+            int recordExists = (int)checkRecord.ExecuteScalar();
+
+            connection.Close();
+
+            return recordExists;
+        }
+
+        public void DeleteRecord()
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            string deleteQuery = "DELETE FROM ATTENDANCE WHERE Attendance_ID = '" + attendanceID + "'";
+
+            command = new SqlCommand(deleteQuery, connection);
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        private void btnDeleteRecord_Click(object sender, EventArgs e)
+        {
+            attendanceID = txtAttendanceID.Text;
+
+            if(attendanceID == "")
+            {
+                lblErrorMessage.ForeColor = System.Drawing.Color.Red;
+                lblErrorMessage.Text = "Please enter a valid Attendance ID!";
+            }
+            else
+            {
+                try
+                {
+                    if(CheckIfRecordExists() == 1)
+                    {
+                        DeleteRecord();
+                        DisplayTable();
+                        ClearFields();
+                    }
+                    else
+                    {
+                        MessageBox.Show("The attendance record you are trying to delete does not exist!\nPlease enter the Attendance ID of a valid record.");
+                    }
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayTable();
+        }
+
+        private void Delete_Attendance_Load(object sender, EventArgs e)
+        {
+            DisplayTable();
+            lblErrorMessage.Text = "";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+    }
+}
---
+++ Student Attendance System/Student Attendance System/Delete Attendance.designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Test
+{
+    partial class Delete_Attendance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDeleteRecord = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAttendanceID = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblErrorMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDeleteRecord
+            // 
+            this.btnDeleteRecord.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDeleteRecord.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnDeleteRecord.ForeColor = System.Drawing.Color.Purple;
+            this.btnDeleteRecord.Location = new System.Drawing.Point(159, 322);
+            this.btnDeleteRecord.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDeleteRecord.Name = "btnDeleteRecord";
+            this.btnDeleteRecord.Size = new System.Drawing.Size(232, 57);
+            this.btnDeleteRecord.TabIndex = 93;
+            this.btnDeleteRecord.Text = "Delete Record";
+            this.btnDeleteRecord.UseVisualStyleBackColor = false;
+            this.btnDeleteRecord.Click += new System.EventHandler(this.btnDeleteRecord_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(17, 257);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(551, 20);
+            this.label11.TabIndex = 99;
+            this.label11.Text = "Please enter the Attendance ID of the record you want to delete:";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnRefresh.ForeColor = System.Drawing.Color.Purple;
+            this.btnRefresh.Location = new System.Drawing.Point(712, 257);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(160, 40);
+            this.btnRefresh.TabIndex = 94;
+            this.btnRefresh.Text = "Refresh Table";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(851, 190);
+            this.dataGridView1.TabIndex = 98;
+            // 
+            // txtAttendanceID
+            // 
+            this.txtAttendanceID.Location = new System.Drawing.Point(159, 292);
+            this.txtAttendanceID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAttendanceID.Name = "txtAttendanceID";
+            this.txtAttendanceID.Size = new System.Drawing.Size(232, 22);
+            this.txtAttendanceID.TabIndex = 92;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(17, 292);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(134, 20);
+            this.label9.TabIndex = 97;
+            this.label9.Text = "Attendance ID:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(24, 99);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 20);
+            this.label10.TabIndex = 96;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(571, 48);
+            this.label1.TabIndex = 95;
+            this.label1.Text = "Delete an attendance record";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnClear.ForeColor = System.Drawing.Color.Purple;
+            this.btnClear.Location = new System.Drawing.Point(399, 292);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(93, 87);
+            this.btnClear.TabIndex = 102;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblErrorMessage
+            // 
+            this.lblErrorMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorMessage.Location = new System.Drawing.Point(500, 322);
+            this.lblErrorMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblErrorMessage.Name = "lblErrorMessage";
+            this.lblErrorMessage.Size = new System.Drawing.Size(372, 57);
+            this.lblErrorMessage.TabIndex = 103;
+            this.lblErrorMessage.Text = "[Error message]";
+            // 
+            // Delete_Attendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 390);
+            this.Controls.Add(this.lblErrorMessage);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDeleteRecord);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtAttendanceID);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label1);
+            this.Name = "Delete_Attendance";
+            this.Text = "Delete_Attendance";
+            this.Load += new System.EventHandler(this.Delete_Attendance_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeleteRecord;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.TextBox txtAttendanceID;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblErrorMessage;
+    }
+}
