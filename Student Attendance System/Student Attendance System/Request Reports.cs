ee7e71690dc71f225ce464124f861c73f5517a34
Erik Bernhardt <30659124@student.g.nwu.ac.za>
2020-10-17 09:06:22 +0200

    commit request reports

--- Student Attendance System/Student Attendance System/Request Reports.cs
+++ Student Attendance System/Student Attendance System/Request Reports.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Student_Attendance_System
 {
@@ -15,6 +16,46 @@ namespace Student_Attendance_System
         public Request_Reports()
         {
             InitializeComponent();
+        }
+
+        //declare and initialize the connection string
+        public string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\bernh\Source\Repos\CMPG223-Project\Student Attendance System\Student Attendance System\Database1.mdf;Integrated Security=True";
+
+        //using System.Data.SqlClient namespace
+        public SqlConnection connection;
+        public SqlCommand command;
+        public SqlDataAdapter adapter;
+        public DataSet dataset;
+
+
+        private void Request_Reports_Load(object sender, EventArgs e)
+        {
+            //query statement
+            string selectAllQuery = "SELECT * ATTENDANCE";
+
+            connection = new SqlConnection(connectionString);
+
+            //open the connection
+            connection.Open();
+
+            command = new SqlCommand(selectAllQuery, connection);
+
+            dataset = new DataSet();
+            adapter = new SqlDataAdapter();
+
+            adapter.SelectCommand = command;
+            adapter.Fill(dataset, "ATTENDANCE");
+
+            dataGridView1.DataSource = dataset;
+            dataGridView1.DataMember = "ATTENDANCE";
+
+            //close the connection
+            connection.Close();
+        }
+
+        private void btnShowAttendance_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
