c21552fe2c89951b054f753ad048cc1489872eb2
FRHeyman <65164502+FRHeyman@users.noreply.github.com>
2020-10-18 19:54:00 +0200

    Maintain Modules

--- Student Attendance System/Student Attendance System/Maintain Modules.Designer.cs
+++ Student Attendance System/Student Attendance System/Maintain Modules.Designer.cs
@@ -40,6 +40,14 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.dgvModules = new System.Windows.Forms.DataGridView();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtMID = new System.Windows.Forms.TextBox();
+            this.txtMDescription = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModules)).BeginInit();
             this.SuspendLayout();
             // 
             // btnReturnToMenu
@@ -51,7 +59,7 @@
             this.btnReturnToMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnReturnToMenu.Name = "btnReturnToMenu";
             this.btnReturnToMenu.Size = new System.Drawing.Size(215, 44);
-            this.btnReturnToMenu.TabIndex = 5;
+            this.btnReturnToMenu.TabIndex = 61;
             this.btnReturnToMenu.Text = "Return To Menu";
             this.btnReturnToMenu.UseVisualStyleBackColor = false;
             this.btnReturnToMenu.Click += new System.EventHandler(this.btnReturnToMenu_Click);
@@ -80,6 +88,7 @@
             this.btnDeleteModule.TabIndex = 4;
             this.btnDeleteModule.Text = "Delete Module";
             this.btnDeleteModule.UseVisualStyleBackColor = false;
+            this.btnDeleteModule.Click += new System.EventHandler(this.btnDeleteModule_Click);
             // 
             // btnAddModule
             // 
@@ -176,11 +185,98 @@
             this.label1.TabIndex = 62;
             this.label1.Text = "Module ID:";
             // 
+            // dgvModules
+            // 
+            this.dgvModules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvModules.Location = new System.Drawing.Point(491, 124);
+            this.dgvModules.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvModules.Name = "dgvModules";
+            this.dgvModules.RowHeadersWidth = 51;
+            this.dgvModules.Size = new System.Drawing.Size(541, 286);
+            this.dgvModules.TabIndex = 63;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnSelect.ForeColor = System.Drawing.Color.Purple;
+            this.btnSelect.Location = new System.Drawing.Point(491, 439);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(228, 44);
+            this.btnSelect.TabIndex = 54;
+            this.btnSelect.Text = "Select Module";
+            this.btnSelect.UseVisualStyleBackColor = false;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtMID
+            // 
+            this.txtMID.Location = new System.Drawing.Point(491, 73);
+            this.txtMID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMID.MaxLength = 25;
+            this.txtMID.Name = "txtMID";
+            this.txtMID.Size = new System.Drawing.Size(201, 22);
+            this.txtMID.TabIndex = 64;
+            this.txtMID.TextChanged += new System.EventHandler(this.txtMID_TextChanged);
+            // 
+            // txtMDescription
+            // 
+            this.txtMDescription.Location = new System.Drawing.Point(831, 73);
+            this.txtMDescription.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMDescription.MaxLength = 25;
+            this.txtMDescription.Name = "txtMDescription";
+            this.txtMDescription.Size = new System.Drawing.Size(201, 22);
+            this.txtMDescription.TabIndex = 65;
+            this.txtMDescription.TextChanged += new System.EventHandler(this.txtMDescription_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(487, 99);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 20);
+            this.label5.TabIndex = 66;
+            this.label5.Text = "Module ID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(827, 99);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 20);
+            this.label7.TabIndex = 67;
+            this.label7.Text = "Description";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnUpdate.ForeColor = System.Drawing.Color.Purple;
+            this.btnUpdate.Location = new System.Drawing.Point(124, 194);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(220, 44);
+            this.btnUpdate.TabIndex = 68;
+            this.btnUpdate.Text = "Update Module";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
             // Maintain_Modules
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1045, 496);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMDescription);
+            this.Controls.Add(this.txtMID);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.dgvModules);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnReturnToMenu);
             this.Controls.Add(this.label8);
@@ -196,6 +292,8 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Maintain_Modules";
             this.Text = "Maintain_Modules";
+            this.Load += new System.EventHandler(this.Maintain_Modules_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModules)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,5 +313,12 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvModules;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtMID;
+        private System.Windows.Forms.TextBox txtMDescription;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
